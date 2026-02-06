a342fa5a28cfa389491ed0807b5a38c941c87eca
dimitris-thomopoulos <thomopoulos59@gmail.com>
2026-02-06 21:50:07 +0100

    second commit - configured methods in controller

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,9 +1,9 @@
+from swagger_server.service.student_service import *
+
 import connexion
-import six
-
-from swagger_server.models.student import Student  # noqa: E501
-from swagger_server import util
-
+# Ensure you import the service functions and the model
+# from swagger_server.models.student import Student
+# from swagger_server.service.student_service import add, delete, get_by_id
 
 def add_student(body=None):  # noqa: E501
     """Add a new student
@@ -13,11 +13,12 @@ def add_student(body=None):  # noqa: E501
     :param body: Student item to add
     :type body: dict | bytes
 
-    :rtype: str
+    :rtype: float
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +31,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +44,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
